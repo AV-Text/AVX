da0f0bdec5193e20b444e2032bd91b3e1c9df7c5
Kevin Wonus <kevin@wonus.com>
2023-06-13 00:26:26 -0400

    Initial adaptation to PEG 2.0.3.612 schema

--- AVXSearch/AVXSearch.h
+++ AVXSearch/AVXSearch.h
@@ -11,14 +11,96 @@ using namespace XBlueprintBlue;
 
 class AVXSettings
 {
+private:
+    static byte similarity_from_input(const XSettings* settings)
+    {
+        std:string value = settings->similarity()->c_str();
+        auto len = value.length();
+        if (len > 0)
+        {
+            if (value.c_str()[len-1] == '!')
+                value = value.substr(0, --len); 
+        }
+        int ival = 0;
+        if (len > 0)
+        {
+            if (value == "exact")
+                ival = 100;
+            else if (value == "none")
+                ival = 0;
+            else
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    ival *= 10;
+
+                    char c = value[i];
+                    if (c >= '0' && c <= '9')
+                        ival += (int)(c - '0');
+                    else
+                        return (byte) 0;
+
+                    if (ival > XThreshold::XThreshold_EXACT)
+                        return (byte) 0;
+                }
+                return ival >= XThreshold::XThreshold_FUZZY_MIN && ival <= XThreshold::XThreshold_EXACT ? (byte)ival : (byte)0;
+            }
+        }
+        return (byte)0;
+    }
+    static bool lemma_matching_from_input(const XSettings* settings)
+    {
+        bool lemma_matching = (settings->lexicon() != XLexEnum::XLexEnum_AV);
+
+        if (lemma_matching)
+        {
+            std:string value = settings->similarity()->c_str();
+            auto len = value.length();
+            if (len > 0)
+            {
+                if (value.c_str()[len - 1] == '!')
+                    return false;
+            }
+            int ival = 0;
+            if (len > 0)
+            {
+                if (value == "exact")
+                    return true;
+                else if (value == "none")
+                    return false;
+                else
+                {
+                    for (int i = 0; i < len; i++)
+                    {
+                        ival *= 10;
+
+                        char c = value[i];
+                        if (c >= '0' && c <= '9')
+                            ival += (int)(c - '0');
+                        else
+                            return false;
+
+                        if (ival > XThreshold::XThreshold_EXACT)
+                            return false;
+                    }
+                    return (ival >= XThreshold::XThreshold_FUZZY_MIN) && (ival <= XThreshold::XThreshold_EXACT) ? true : false;
+                }
+            }
+        }
+        return false;
+    }
 public:
     const XLexEnum lexicon;
-    const bool exact;
+    const XOutEnum display;
+    const byte similarity;
+    const bool automatic_lemma_matching;
     const uint16 span;
     const XFmtEnum format;
     AVXSettings(const XSettings* settings) :
         lexicon(settings->lexicon()),
-        exact(settings->exact()),
+        display(settings->display()),
+        similarity(AVXSettings::similarity_from_input(settings)),
+        automatic_lemma_matching(AVXSettings::lemma_matching_from_input(settings)),
         span(settings->span()),
         format(settings->format())
     {
